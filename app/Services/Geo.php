c2be512c8797856e227eb189ac3fcb2320eb0bd0
Aslam <forjob.aslam@gmail.com>
2025-04-22 23:23:08 +0700

    add facade geo to estimating time from base to target

---
+++ app/Services/Geo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class Geo
+{
+    public function haversineDistance($lat1, $lon1, $lat2, $lon2)
+    {
+        $earthRadius = 6371;
+
+        $dLat = deg2rad($lat2 - $lat1);
+        $dLon = deg2rad($lon2 - $lon1);
+
+        $a = sin($dLat / 2) ** 2 +
+            cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+            sin($dLon / 2) ** 2;
+
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        return $earthRadius * $c;
+    }
+}
